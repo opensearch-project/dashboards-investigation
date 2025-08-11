3b52238a6b2c81701fa8d580c54de058519bd431
SuZhou-Joe <suzhou@amazon.com>
2025-08-11 13:44:46 +0800

    feat: update
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,11 +4,10 @@
  */
 
 import { IRouter } from '../../../../src/core/server';
-import { registerNoteRoute } from './notebooks/noteRouter';
-import { registerParaRoute } from './notebooks/paraRouter';
-import { registerSqlRoute } from './notebooks/sqlRouter';
-import { registerVizRoute } from './notebooks/vizRouter';
-import { registerLogPatternRoute } from './notebooks/logPatternRouter';
+import { registerNoteRoute } from './notebooks/notebook_router';
+import { registerParaRoute } from './notebooks/paragraph_router';
+import { registerSqlRoute } from './notebooks/sql_router';
+import { registerLogPatternRoute } from './notebooks/log_pattern_router';
 import { getQueryService } from '../services/get_set';
 
 export function setupRoutes({
@@ -21,7 +20,6 @@ export function setupRoutes({
   // notebooks routes
   registerParaRoute(router);
   registerNoteRoute(router);
-  registerVizRoute(router, dataSourceEnabled);
   registerLogPatternRoute(router);
 
   registerSqlRoute(router, getQueryService(), dataSourceEnabled);
