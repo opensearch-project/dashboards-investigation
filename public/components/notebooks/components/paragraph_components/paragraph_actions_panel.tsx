e34c224284cf7d21220d41d377f46c03b51ada56
Zhenxing Shen <zxshen@amazon.com>
2025-08-22 16:28:52 +0800

    enable delete action
    Signed-off-by: Zhenxing Shen <zxshen@amazon.com>

--- public/components/notebooks/components/paragraph_components/paragraph_actions_panel.tsx
+++ public/components/notebooks/components/paragraph_components/paragraph_actions_panel.tsx
@@ -82,7 +82,6 @@ export const ParagraphActionPanel = (props: {
         },
         {
           name: 'Delete',
-          disabled: idx <= paragraphStates.length - 1 || notebookType === NotebookType.AGENTIC,
           onClick: () => {
             setIsPopoverOpen(false);
             props.deletePara(idx);
