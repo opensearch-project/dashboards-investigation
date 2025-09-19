0cfd9b4171295e3e6ea8477cd118d0d699d5c26c
Owen Wang <owenwyk@amazon.com>
2025-09-19 12:03:35 +0800

    Add hypothesis detail page (#125)
    * Add hypothesis detail page
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>
    
    * Address comments
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>
    
    ---------
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>

--- public/components/notebooks/components/main.tsx
+++ public/components/notebooks/components/main.tsx
@@ -94,7 +94,6 @@ export const Main: React.FC = () => {
             render={(_props) => <NoteTable deleteNotebook={deleteNotebook} />}
           />
           <Route
-            exact
             path="/:id"
             render={(props) => {
               return <Notebook openedNoteId={props.match.params.id} showPageHeader />;
