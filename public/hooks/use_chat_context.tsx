4238f50dcafd3c206fb03cea7a2d2b87870670aa
Lin Wang <wonglam@amazon.com>
2025-11-14 15:27:34 +0800

    Only pass markdown paragraphs to avoid context too large (#204)
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/hooks/use_chat_context.tsx
+++ public/hooks/use_chat_context.tsx
@@ -8,7 +8,6 @@ import { useObservable } from 'react-use';
 import { NoteBookServices } from 'public/types';
 import { useOpenSearchDashboards } from '../../../../src/plugins/opensearch_dashboards_react/public';
 import { NotebookReactContext } from '../components/notebooks/context_provider/context_provider';
-import { SubRouter, useSubRouter } from './use_sub_router';
 import { generateParagraphPrompt } from '../services/helpers/per_agent';
 
 export const useChatContextProvider = () => {
@@ -16,7 +15,6 @@ export const useChatContextProvider = () => {
   const {
     services: { updateContext, paragraphService },
   } = useOpenSearchDashboards<NoteBookServices>();
-  const { page } = useSubRouter();
   const { context: topLevelContext, title, id, paragraphs, hypotheses } = context.state.value;
   const topLevelContextValue = useObservable(topLevelContext.getValue$(), topLevelContext.value);
 
@@ -35,46 +33,42 @@ export const useChatContextProvider = () => {
   }, [hypotheses]);
 
   useEffect(() => {
-    // Invesitagation metadata
-    updateContext(`Investigation-${id}`, {
-      label: `Investigation: ${title}`,
-      description: 'Metadata information for the investigation',
-      value: {
-        notebookId: id,
-        topLevelContext: topLevelContextValue,
-      },
-      categories: ['chat', 'investigation'],
-    });
+    let changed = false;
+    const contextId = `Investigation-${id}`;
 
     async function updateContextWithParagraphs() {
       const paragraphPrompt = await generateParagraphPrompt({
         paragraphService,
-        paragraphs: paragraphs.map((para) => para.value),
+        paragraphs: paragraphs
+          // Only MARKDOWN paragraph allowed to avoid context too large
+          .filter((paragraph) => ['MARKDOWN'].includes(paragraph.getParagraphType()))
+          .map((para) => para.value),
       });
+      if (changed) {
+        return;
+      }
       const findingsContext = `## Findings\n\n${paragraphPrompt.filter((item) => item).join('\n')}`;
-      updateContext(`Investigation-${id}-findings`, {
-        label: `Hypothesis & Findings: ${title}`,
-        description: 'Hypothesis and findings information for current investigation',
+      updateContext(contextId, {
+        label: `Investigation: ${title}`,
+        description: 'Metadata, hypotheses and findings for the investigation',
         value: {
           notebookId: id,
-          paragraphContext: hypothesesContext + '\n\n' + findingsContext,
+          metadata: topLevelContextValue,
+          hypotheses: hypothesesContext,
+          findings: findingsContext,
         },
-        categories: ['chat', 'investigation', 'hypothesis', 'finding'],
+        categories: ['chat', 'investigation'],
       });
     }
 
-    // Add paragraphs to context only for investigation page
-    if (paragraphs.length > 0 && page === SubRouter.Investigation) {
-      updateContextWithParagraphs();
-    }
+    updateContextWithParagraphs();
     return () => {
-      updateContext(`Investigation-${id}`, undefined);
-      updateContext(`Investigation-${id}-findings`, undefined);
+      changed = true;
+      updateContext(contextId, undefined);
     };
   }, [
     updateContext,
     title,
-    page,
     id,
     topLevelContextValue,
     paragraphService,
