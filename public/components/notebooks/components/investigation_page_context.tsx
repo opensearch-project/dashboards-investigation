b8e154fe9bdd5209f258c20f8d34ba31abd2f77b
Lin Wang <wonglam@amazon.com>
2025-12-02 16:33:34 +0800

    Add investigation page context for agentic notebook
    Signed-off-by: Lin Wang <wonglam@amazon.com>

---
+++ public/components/notebooks/components/investigation_page_context.tsx
@@ -0,0 +1,29 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { investigationNotebookID } from '../../../../common/constants/shared';
+import type { NoteBookServices } from '../../../types';
+
+export interface InvestigationPageContextProps {
+  usePageContext: Required<NoteBookServices>['contextProvider']['hooks']['usePageContext'];
+  dataSourceId?: string;
+}
+
+export const InvestigationPageContext = ({
+  usePageContext,
+  dataSourceId,
+}: InvestigationPageContextProps) => {
+  usePageContext({
+    description: 'Investigation notebooks application page context',
+    convert: () => ({
+      appId: investigationNotebookID,
+      dataset: {
+        dataSource: { id: dataSourceId },
+      },
+    }),
+    categories: ['static', investigationNotebookID],
+  });
+  return null;
+};
