194f745f65273d3d630df0c7f9e1c73b7776ff39
Owen Wang <owenwyk@amazon.com>
2025-12-19 15:43:26 +0800

    Remove unnecessary dependencies for investigation notebook (#263)
    * Remove unnecessary dependencies for investigation notebook
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>
    
    * Remove react markdown
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>
    
    * Add dompurify to dependencies
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>
    
    ---------
    
    Signed-off-by: Owen Wang <owenwyk@amazon.com>

--- public/components/notebooks/components/hypothesis/investigation/message_trace_flyout.tsx
+++ public/components/notebooks/components/hypothesis/investigation/message_trace_flyout.tsx
@@ -4,7 +4,6 @@
  */
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import moment from 'moment';
-import MarkdownRender from '@nteract/markdown';
 import {
   EuiAccordion,
   EuiText,
@@ -16,6 +15,7 @@ import {
   EuiFlyoutHeader,
   EuiTitle,
   EuiFlyoutBody,
+  EuiMarkdownFormat,
 } from '@elastic/eui';
 import { useObservable } from 'react-use';
 import { timer } from 'rxjs';
@@ -50,7 +50,7 @@ const renderTraceString = ({ text, fallback }: { text: string | undefined; fallb
   }
 
   return isMarkdownText(text) ? (
-    <MarkdownRender source={text} />
+    <EuiMarkdownFormat>{text}</EuiMarkdownFormat>
   ) : (
     <EuiCodeBlock isCopyable>{text}</EuiCodeBlock>
   );
