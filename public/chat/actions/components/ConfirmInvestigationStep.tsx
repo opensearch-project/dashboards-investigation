9a28c3214b011d92ff11c0e243ddb4c75fb50597
Hailong Cui <ihailong@amazon.com>
2026-01-20 10:21:44 +0800

    UI adjustment
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- public/chat/actions/components/ConfirmInvestigationStep.tsx
+++ public/chat/actions/components/ConfirmInvestigationStep.tsx
@@ -57,116 +57,120 @@ export const ConfirmInvestigationStep: React.FC<Props> = ({
   };
 
   return (
-    <>
-      <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
-        <EuiFlexItem grow={false}>
-          {isComplete ? (
-            <EuiIcon type="checkInCircleEmpty" color="success" />
-          ) : (
-            <EuiLoadingSpinner size="m" />
-          )}
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiText size="s">
-            <strong>Confirm investigation details</strong>
-          </EuiText>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-
-      {/* Investigation Details Panel */}
-      <EuiSplitPanel.Outer hasBorder={false} hasShadow={false}>
-        <EuiSplitPanel.Inner paddingSize="s">
-          <EuiFlexGroup direction="column" gutterSize="m">
-            <EuiFlexItem>
-              <EuiText size="s">
-                <strong>Goal</strong>
-              </EuiText>
-              <EuiSpacer size="xs" />
-              <EuiText size="s" color="subdued">
-                {data.initialGoal}
-              </EuiText>
-            </EuiFlexItem>
-
-            <EuiFlexItem>
-              <EuiText size="s">
-                <strong>Symptom</strong>
-              </EuiText>
-              <EuiSpacer size="xs" />
-              <EuiText size="s" color="subdued">
-                {data.symptom}
-              </EuiText>
-            </EuiFlexItem>
-
-            <EuiFlexItem>
-              <EuiText size="s">
-                <strong>Index</strong>
-              </EuiText>
-              <EuiSpacer size="xs" />
-              <EuiText size="s" color="subdued">
-                {data.index}
-              </EuiText>
-            </EuiFlexItem>
-
-            {data.timeRange && (
-              <EuiFlexItem>
-                <EuiText size="s">
-                  <strong>Time range</strong>
-                </EuiText>
-                <EuiSpacer size="xs" />
-                <EuiText size="s" color="subdued">
-                  {formatTimeRange(data.timeRange)}
-                </EuiText>
-              </EuiFlexItem>
-            )}
-          </EuiFlexGroup>
-        </EuiSplitPanel.Inner>
-
-        {!isComplete && (
-          <EuiSplitPanel.Inner color="subdued" paddingSize="s">
-            <EuiFlexGroup justifyContent="spaceBetween" alignItems="center" gutterSize="s">
-              <EuiFlexItem>
-                <EuiText size="s">
-                  <strong>Investigation details</strong>
-                </EuiText>
-              </EuiFlexItem>
+    <EuiFlexGroup direction="column" gutterSize="none">
+      <EuiFlexItem grow={true}>
+        {/* Investigation Details Panel */}
+        <EuiSplitPanel.Outer hasBorder={true} hasShadow={false}>
+          <EuiSplitPanel.Inner paddingSize="s">
+            <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
               <EuiFlexItem grow={false}>
-                <EuiFlexGroup gutterSize="s" responsive={false}>
-                  {onEdit && (
-                    <EuiFlexItem grow={false}>
-                      <EuiButtonIcon
-                        iconType="pencil"
-                        aria-label="Edit investigation details"
-                        onClick={onEdit}
-                        color="text"
-                      />
-                    </EuiFlexItem>
-                  )}
-                  {onCancel && (
-                    <EuiFlexItem grow={false}>
-                      <EuiButtonIcon
-                        iconType="crossInCircleEmpty"
-                        aria-label="Cancel investigation"
-                        onClick={onCancel}
-                        color="danger"
-                      />
-                    </EuiFlexItem>
-                  )}
-                  {onConfirm && (
-                    <EuiFlexItem grow={false}>
-                      <EuiButtonIcon
-                        iconType="checkInCircleEmpty"
-                        aria-label="Confirm investigation"
-                        onClick={onConfirm}
-                        color="success"
-                      />
-                    </EuiFlexItem>
-                  )}
-                </EuiFlexGroup>
+                {isComplete ? (
+                  <EuiIcon type="checkInCircleEmpty" color="success" />
+                ) : (
+                  <EuiLoadingSpinner size="m" />
+                )}
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiText size="s">
+                  <strong>Confirm investigation details</strong>
+                </EuiText>
               </EuiFlexItem>
             </EuiFlexGroup>
           </EuiSplitPanel.Inner>
-        )}
-      </EuiSplitPanel.Outer>
-    </>
+
+          <EuiSplitPanel.Inner paddingSize="s">
+            <EuiFlexGroup direction="column" gutterSize="m">
+              <EuiFlexItem>
+                <EuiText size="s">
+                  <strong>Goal</strong>
+                </EuiText>
+                <EuiSpacer size="xs" />
+                <EuiText size="s" color="subdued">
+                  {data.initialGoal}
+                </EuiText>
+              </EuiFlexItem>
+
+              <EuiFlexItem>
+                <EuiText size="s">
+                  <strong>Symptom</strong>
+                </EuiText>
+                <EuiSpacer size="xs" />
+                <EuiText size="s" color="subdued">
+                  {data.symptom}
+                </EuiText>
+              </EuiFlexItem>
+
+              <EuiFlexItem>
+                <EuiText size="s">
+                  <strong>Index</strong>
+                </EuiText>
+                <EuiSpacer size="xs" />
+                <EuiText size="s" color="subdued">
+                  {data.index}
+                </EuiText>
+              </EuiFlexItem>
+
+              {data.timeRange && (
+                <EuiFlexItem>
+                  <EuiText size="s">
+                    <strong>Time range</strong>
+                  </EuiText>
+                  <EuiSpacer size="xs" />
+                  <EuiText size="s" color="subdued">
+                    {formatTimeRange(data.timeRange)}
+                  </EuiText>
+                </EuiFlexItem>
+              )}
+            </EuiFlexGroup>
+          </EuiSplitPanel.Inner>
+
+          {!isComplete && (
+            <EuiSplitPanel.Inner color="subdued" paddingSize="s">
+              <EuiFlexGroup justifyContent="spaceBetween" alignItems="center" gutterSize="s">
+                <EuiFlexItem>
+                  <EuiText size="s">
+                    <strong>Investigation details</strong>
+                  </EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="s" responsive={false}>
+                    {onEdit && (
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonIcon
+                          iconType="pencil"
+                          aria-label="Edit investigation details"
+                          onClick={onEdit}
+                          color="text"
+                        />
+                      </EuiFlexItem>
+                    )}
+                    {onCancel && (
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonIcon
+                          iconType="crossInCircleEmpty"
+                          aria-label="Cancel investigation"
+                          onClick={onCancel}
+                          color="danger"
+                        />
+                      </EuiFlexItem>
+                    )}
+                    {onConfirm && (
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonIcon
+                          iconType="checkInCircleEmpty"
+                          aria-label="Confirm investigation"
+                          onClick={onConfirm}
+                          color="success"
+                        />
+                      </EuiFlexItem>
+                    )}
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiSplitPanel.Inner>
+          )}
+        </EuiSplitPanel.Outer>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 };
